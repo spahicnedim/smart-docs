21fb61476c65b96fb28296ca674b99f1b5e2e139
spahicnedim <spahicnedim3@gmail.com>
2026-05-02 15:09:48 +0200

    fix in nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    turbopack: {},
     output: 'standalone',
+    serverExternalPackages: ['pdf-parse', 'unpdf', 'better-sqlite3'],
 };
 
 export default nextConfig;
